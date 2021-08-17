7b7b7900e45fea975e000b1087beadf7bd3f0f8d
Boris Boinier <saukratesk@gmail.Com>
2021-08-17 13:56:58 +0200

    Parametrage de ML Flow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,6 +1,17 @@
-# imports
+import mlflow
+from  mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 
 class Trainer():
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -10,15 +21,13 @@ class Trainer():
         self.set_pipeline()
         self.X = X
         self.y = y
+        self.experiment_name = "[FR] [Nantes] [saukratesk] TaxiFareModel v1"
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
         '''returns a pipelined model'''
-        from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
-        from sklearn.preprocessing import StandardScaler, OneHotEncoder
-        from sklearn.linear_model import LinearRegression
-        from sklearn.pipeline import Pipeline
-        from sklearn.compose import ColumnTransformer
+
 
 
         dist_pipe = Pipeline([
@@ -53,8 +62,33 @@ class Trainer():
 
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
+        #print(f'{self.pipeline.get_params()}')
+        self.mlflow_log_param("model", "linear")
         print(rmse)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 from TaxiFareModel.data import get_data, clean_data
@@ -78,8 +112,8 @@ if __name__ == "__main__":
 
     # train
     new_trainer.set_pipeline()
-    self.X = X_train
-    self.y = y_train
+    new_trainer.X = X_train
+    new_trainer.y = y_train
 
     new_trainer.run()
 
